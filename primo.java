a57bdb9adf439f2d2d86290985dc59da08a94e59
EnzoCosmos <142914537+EnzoCosmos@users.noreply.github.com>
2023-11-21 09:16:42 -0300

    Update primo.java

--- primo.java
+++ primo.java
@@ -1,5 +1,4 @@
 //problema 3
-package vetor;
 import java.util.Scanner; 
 public class primo {
 
@@ -15,15 +14,15 @@ public class primo {
         vetorA[i] = scanner.nextInt();
     }
 
-    System.out.println("Elementos do vetor A e sua natureza (primo ou n„o):");
+    System.out.println("Elementos do vetor A e sua natureza (primo ou n√£o):");
 
     for (int i = 0; i < vetorA.length; i++) {
         System.out.print("Elemento " + vetorA[i] + ": ");
 
         if (ehPrimo(vetorA[i])) {
-            System.out.println("… um n˙mero primo");
+            System.out.println("√â um n√∫mero primo");
         } else {
-            System.out.println("N„o È um n˙mero primo");
+            System.out.println("N√£o √© um n√∫mero primo");
         }
     }
 
